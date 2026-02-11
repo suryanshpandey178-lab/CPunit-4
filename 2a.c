f8cd3439afdf7c2370a8cb6d9004b77796c2ed2b
Suryansh Pandey <suryanshpandey178@gmail.com>
2026-02-11 08:17:28 +0530

    Update 2a.c

--- 2a.c
+++ 2a.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 int main() {
     FILE *fp;
-    fp = fopen("example.txt", "w");  // create or open file for writing
+    fp = fopen("example.txt", "w");
     if(fp == NULL) {
         printf("File opening failed!\n");
         return 1;
     }
     printf("File opened successfully using fopen().\n");
-    fclose(fp); // closing the file
+    fclose(fp); 
     printf("File closed successfully using fclose().\n");
     return 0;
 }
